a9f9323a5a94957a02f86a43e4600fc0697d4a96
Andreycska <pashenkoandrew@gmail.com>
2020-11-30 16:42:24 +0300

    Smooth menu efect for js

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ $(function() {
     let introH = $("#intro").innerHeight();
     let scrollOffset = $(window).scrollTop();
 
+    // HEADER-FIXED
+
     checkScroll(scrollOffset);
 
     $(window).on("scroll", function() {
@@ -20,5 +22,19 @@ $(function() {
             header.removeClass("fixed");
         }
     }
+
+    // SMOOTH SCROLL
+
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+        let blockId = $(this).data("scroll");
+        let blockOffset = $(blockId).offset().top;
+
+        $("#nav a").removeClass("active");
+        $(this).addClass("active");
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 500)
+    });
 
 });
